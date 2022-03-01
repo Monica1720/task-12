d3b50d715d24b50a67504cde285008c5795430c4
Monica1720 <100676391+Monica1720@users.noreply.github.com>
2022-03-01 15:32:52 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_db_instance" "task12" {
+  allocated_storage    = 10
+  engine               = "postgres"
+  identifier           =  "database-db"     
+  engine_version       = "13"
+  instance_class       = "db.t3.medium"
+  name                 =  var.name
+  username             =  var.username
+  password             =  var.password
+  vpc_security_group_ids = var.sg
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+}
---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "name" {
+default = "monica"
+}
+variable "username" {
+default = "monica"
+}
+variable "password" {
+default = "monica1720"
+}
+variable "sg" {
+  type = list
+  default = ["sg-0343af50cdc4e3565"]
+}
